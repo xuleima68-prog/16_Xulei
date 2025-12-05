9b370c599f8ec48b568a789196f1c5ea21c825ea
xuleima68-prog <xuleima68@gmail.com>
2025-12-05 13:48:19 +0100

    js

---
+++ 4/pueba1.js
@@ -0,0 +1,14 @@
+//comentario de una sola linea
+/*comentario
+de varias lineas*/
+// alert("Prueba de codigo")
+var mensaje = "hola mundooooooooooo";
+console.log(mensaje);
+console.log("Hola mundo");
+console.log(document);
+console.log(localStorage);
+var esViernes = true;  
+console.log("Es viernes " + esViernes);
+if (esViernes == true) {
+    console.log("Por fin es viernes vete a la meir Guillermo");
+}
