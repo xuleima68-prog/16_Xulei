af87491fe2ad82aa6dc28ffdaf5de0b686a80cd7
Xulei <xuleima55@gmail.com>
2025-12-13 21:52:33 +0100

    promedio

--- 4/4.2.js
+++ 4/4.2.js
@@ -1,13 +1,12 @@
 function enviarFormulario() {
     var nota1 = document.getElementById("nota1").value;
     var nota2 = document.getElementById("nota2").value;
-    var nota3 = document.getElementById("nota3").value;
 
     var promedio = (parseFloat(nota1) + parseFloat(nota2)) / 2;
     console.log("Promedio: " + promedio.toFixed(2));
 
 if (comprobarNota()) {
-    var nombre = document.getElementById("nombre").value;
+    var promedio = document.getElementById("promedio").value;
     div.innerHTML = "<h1>Notable</h1>"; //en vez de escribir en el html document, lo escribimos en el div desde la apertura al cierre
     div.style.backgroundColor = "green";
     }
